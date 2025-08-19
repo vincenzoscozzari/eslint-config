609bcc3a5108d9940b58735d23274266a427474b
Vincenzo Scozzari <vincenzo.scozzari@comersud.it>
2025-08-19 15:26:05 +0200

    feat(index.js): add eslint-config

---
+++ index.js
@@ -0,0 +1,31 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: { version: 'detect' },
+    },
+    languageOptions: {
+      globals: { route: 'readonly' },
+    },
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      'no-undef': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
